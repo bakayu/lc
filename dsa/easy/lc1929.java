0f69b8fd3f9f33ff3ef779da32bc43278df72226
Ayush <mail@ayuch.dev>
2025-08-10 20:21:24 +0530

    add .gitignore

--- dsa/easy/lc1929.java
+++ dsa/easy/lc1929.java
@@ -1,4 +1,4 @@
-class Solution {
+class lc1929 {
     public int[] getConcatenation(int[] nums) {
         int[] arr = new int[(nums.length * 2)];
         for (int i = 0; i < nums.length; i++) {
--- dsa/easy/lc704.java
+++ dsa/easy/lc704.java
@@ -1,9 +1,9 @@
-class Solution {
+class lc704 {
     public int search(int[] nums, int target) {
         int start = 0, end = nums.length - 1;
 
         while (start <= end) {
-            int mid = start + (end - start)/2;
+            int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
