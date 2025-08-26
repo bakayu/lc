4cb4fd91b808385b9af85314e99ab29ecb8964c9
Ayush <mail@ayuch.dev>
2025-08-26 19:02:54 +0530

    lc(easy) - 867
    Signed-off-by: Ayush <mail@ayuch.dev>

---
+++ dsa/easy/lc867.java
@@ -0,0 +1,14 @@
+public class lc867 {
+    public int[][] transpose(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[j][i] = mat[i][j];
+            }
+        }
+
+        return arr;
+    }
+}
